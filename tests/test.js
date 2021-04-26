b99af5bc607c276f459d87cefbcbfe23f436b5ee
Alexandros Diamantonis <asdiamantonis@gmail.com>
2021-04-26 13:42:17 +0200

    cleaned up test

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,5 @@
 import * as jsonData from "../mock_data/data.json";
 import { Navigator as GuildNavigator } from "../dist/index.js";
-// import * as SpacingGuild from "../dist/index.js";
 
 const data = jsonData.default;
 
@@ -10,7 +9,6 @@ const data = jsonData.default;
  * https://en.wikipedia.org/wiki/Dune_(novel)
  */
 const edric = new GuildNavigator(data.locations, data.ship);
-// console.log(edric);
 
 const trade = edric.navigate({ fuelMargin: 10 });
 console.log(trade);
